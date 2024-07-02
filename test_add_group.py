608fe47eb23c0a092ce8c255a2050652130b2cb3
TAlekseeva <TAlekseeva@luxoft.com>
2024-07-02 10:51:24 +0200

    To be continued

--- test_add_group.py
+++ test_add_group.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
@@ -11,29 +12,59 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="TestGroup", header="test11", footer="test22")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element("link text", "Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element("link text", "group page").click()
+
+    def create_group(self, wd, name, header, footer):
+        # init group creation
+        wd.find_element("name", "new").click()
+        # fill group form
+        wd.find_element("name", "group_name").click()
+        wd.find_element("name", "group_name").clear()
+        wd.find_element("name", "group_name").send_keys(name)
+        wd.find_element("name", "group_header").click()
+        wd.find_element("name", "group_header").clear()
+        wd.find_element("name", "group_header").send_keys(header)
+        wd.find_element("name", "group_footer").click()
+        wd.find_element("name", "group_footer").clear()
+        wd.find_element("name", "group_footer").send_keys(footer)
+        # submit group creation
+        wd.find_element("name", "submit").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element("link text", "groups").click()
+
+    def login(self, wd, username, password):
         wd.find_element("name", "user").click()
         wd.find_element("name", "user").clear()
-        wd.find_element("name", "user").send_keys("admin")
+        wd.find_element("name", "user").send_keys(username)
         wd.find_element("id", "LoginForm").click()
         wd.find_element("name", "pass").click()
         wd.find_element("name", "pass").clear()
-        wd.find_element("name", "pass").send_keys("secret")
+        wd.find_element("name", "pass").send_keys(password)
         wd.find_element("css selector", "input[type=\"submit\"]").click()
-        wd.find_element("link text", "groups").click()
-        wd.find_element("name", "new").click()
-        wd.find_element("name", "group_name").click()
-        wd.find_element("name", "group_name").clear()
-        wd.find_element("name", "group_name").send_keys("TestGroup")
-        wd.find_element("name", "group_header").click()
-        wd.find_element("name", "group_header").clear()
-        wd.find_element("name", "group_header").send_keys("test11")
-        wd.find_element("name", "group_footer").click()
-        wd.find_element("name", "group_footer").clear()
-        wd.find_element("name", "group_footer").send_keys("test22")
-        wd.find_element("name", "submit").click()
-        wd.find_element("link text", "group page").click()
-        wd.find_element("link text", "Logout").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
 
     def is_element_present(self, how, what):
         try:
