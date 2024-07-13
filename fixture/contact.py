9c22081a6d5d2a4f288e6c556761eca9cd0026cd
Tatiana Alekseeva <tiikeri0578@gmail.com>
2024-07-13 13:41:27 +0200

    Fixtures refactoring

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,31 +1,38 @@
+def fill_contact_fields(contact, wd):
+    # edit contact form
+    wd.find_element("name", "firstname").click()
+    wd.find_element("name", "firstname").clear()
+    wd.find_element("name", "firstname").send_keys(contact.firstname)
+    wd.find_element("name", "lastname").click()
+    wd.find_element("name", "lastname").clear()
+    wd.find_element("name", "lastname").send_keys(contact.lastname)
+    wd.find_element("name", "email").click()
+    wd.find_element("name", "email").clear()
+    wd.find_element("name", "email").send_keys(contact.email)
+
+
 class ContactHelper:
 
     def __init__(self, app):
         self.app = app
 
-    def return_to_contacts_page(self):
+    def open_contacts_page(self):
         wd = self.app.wd
-        wd.find_element("link text", "home page").click()
+        wd.get("http://localhost/addressbook/")
+        # wd.find_element("link text", "home page").click()
 
     def create(self, contact):
         wd = self.app.wd
         # init contact creation
         wd.find_element("link text", "add new").click()
         # fill contact form
-        wd.find_element("name", "firstname").click()
-        wd.find_element("name", "firstname").clear()
-        wd.find_element("name", "firstname").send_keys(contact.firstname)
-        wd.find_element("name", "lastname").click()
-        wd.find_element("name", "lastname").clear()
-        wd.find_element("name", "lastname").send_keys(contact.lastname)
-        wd.find_element("name", "email").click()
-        wd.find_element("name", "email").clear()
-        wd.find_element("name", "email").send_keys(contact.email)
+        fill_contact_fields(contact, wd)
         # submit contact creation
         wd.find_element("name", "submit").click()
-        self.return_to_contacts_page()
+        self.open_contacts_page()
 
     def delete_first_contact(self):
+        self.open_contacts_page()
         wd = self.app.wd
         # select first contact
         wd.find_element("name", "selected[]").click()
@@ -34,21 +41,12 @@ class ContactHelper:
         wd.find_element("id", "maintable")
 
     def edit_first(self, contact):
+        self.open_contacts_page()
         wd = self.app.wd
         # select first contact
         wd.find_element("name", "selected[]").click()
         wd.find_element("xpath", "//img[@alt='Edit']").click()
-        # edit contact form
-        wd.find_element("name", "firstname").click()
-        wd.find_element("name", "firstname").clear()
-        wd.find_element("name", "firstname").send_keys(contact.firstname)
-        wd.find_element("name", "lastname").click()
-        wd.find_element("name", "lastname").clear()
-        wd.find_element("name", "lastname").send_keys(contact.lastname)
-        wd.find_element("name", "email").click()
-        wd.find_element("name", "email").clear()
-        wd.find_element("name", "email").send_keys(contact.email)
+        fill_contact_fields(contact, wd)
         # submit contact update
         wd.find_element("name", "update").click()
-        self.return_to_contacts_page()
-
+        self.open_contacts_page()
--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,15 @@
+def fill_group_fields(group, wd):
+    wd.find_element("name", "group_name").click()
+    wd.find_element("name", "group_name").clear()
+    wd.find_element("name", "group_name").send_keys(group.name)
+    wd.find_element("name", "group_header").click()
+    wd.find_element("name", "group_header").clear()
+    wd.find_element("name", "group_header").send_keys(group.header)
+    wd.find_element("name", "group_footer").click()
+    wd.find_element("name", "group_footer").clear()
+    wd.find_element("name", "group_footer").send_keys(group.footer)
+
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -13,15 +25,7 @@ class GroupHelper:
         # init group creation
         wd.find_element("name", "new").click()
         # fill group form
-        wd.find_element("name", "group_name").click()
-        wd.find_element("name", "group_name").clear()
-        wd.find_element("name", "group_name").send_keys(group.name)
-        wd.find_element("name", "group_header").click()
-        wd.find_element("name", "group_header").clear()
-        wd.find_element("name", "group_header").send_keys(group.header)
-        wd.find_element("name", "group_footer").click()
-        wd.find_element("name", "group_footer").clear()
-        wd.find_element("name", "group_footer").send_keys(group.footer)
+        fill_group_fields(group, wd)
         # submit group creation
         wd.find_element("name", "submit").click()
         self.return_to_groups_page()
@@ -46,15 +50,7 @@ class GroupHelper:
         wd.find_element("name", "selected[]").click()
         # update group data
         wd.find_element("name", "edit").click()
-        wd.find_element("name", "group_name").click()
-        wd.find_element("name", "group_name").clear()
-        wd.find_element("name", "group_name").send_keys(group.name)
-        wd.find_element("name", "group_header").click()
-        wd.find_element("name", "group_header").clear()
-        wd.find_element("name", "group_header").send_keys(group.header)
-        wd.find_element("name", "group_footer").click()
-        wd.find_element("name", "group_footer").clear()
-        wd.find_element("name", "group_footer").send_keys(group.footer)
+        fill_group_fields(group, wd)
         # submit group update
         wd.find_element("name", "update").click()
         self.return_to_groups_page()
